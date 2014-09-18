22be03f338dfd334f5eda55a95ab6903048ad6c3
georgi.kovachev2014@gmail.com <georgi.kovachev2014@gmail.com>
2014-09-18 10:43:37 +0300

    update several issues

--- geoip.php
+++ geoip.php
@@ -3,26 +3,32 @@ Mage::app();
 $geoIP = Mage::getSingleton('geoip/country');
 $country = $geoIP->getCountry();
 
-if(strcmp($country,'NL') == 0) {
-    $mageRunType = 'store';
-    $mageRunCode = 'nl';
-}
-else if(strcmp($country,'GB') == 0) {
-    $mageRunType = 'store';
-    $mageRunCode = 'en';
-}
-else if(strcmp($country,'FR') == 0) {
-    $mageRunType = 'store';
-    $mageRunCode = 'fr';
-}
-else if(strcmp($country,'DE') == 0) {
-    $mageRunType = 'store';
-    $mageRunCode = 'de';
-}
-else
-{
-    $mageRunType = 'store';
-    $mageRunCode = 'eu';
-}
+
+
+    if(strcmp($country,'NL') == 0) {
+        $mageRunType = 'store';
+        $mageRunCode = 'nl';
+    }
+    else if(strcmp($country,'GB') == 0) {
+        $mageRunType = 'store';
+        $mageRunCode = 'en';
+    }
+    else if(strcmp($country,'FR') == 0) {
+        $mageRunType = 'store';
+        $mageRunCode = 'fr';
+    }
+    else if(strcmp($country,'DE') == 0) {
+        $mageRunType = 'store';
+        $mageRunCode = 'de';
+    }
+    else
+    {
+        $mageRunType = 'store';
+        $mageRunCode = 'eu';
+    }
+
+
+
+
 Mage::reset();
 ?>
