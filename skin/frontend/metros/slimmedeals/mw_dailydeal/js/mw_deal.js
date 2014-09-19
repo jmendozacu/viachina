9d1ead4efe7d56a7f1c0de2475daafe83e6b3ecf
georgi.kovachev2014@gmail.com <georgi.kovachev2014@gmail.com>
2014-09-20 01:30:44 +0300

    more information scroll

--- skin/frontend/metros/slimmedeals/mw_dailydeal/js/mw_deal.js
+++ skin/frontend/metros/slimmedeals/mw_dailydeal/js/mw_deal.js
@@ -1,7 +1,12 @@
 jQuery( window ).load(function() {
     jQuery('.more-info').click(function(){
         var parent = jQuery(this).parent().parent().parent().parent();
-        parent.find('.attribute-specs').toggle();
+        var ele = parent.find('.attribute-specs');
+        ele.toggle();
+
+        var top = ele.position().top;
+        jQuery(window).scrollTop(top);
+
         console.log (jQuery(this).parent().parent().parent().parent());
     });
 });
